b96f95f798213452a27806ef2b71cb115dcecd89
Combi153 <jcm04@naver.com>
2023-11-11 20:30:58 +0900

    [feat] JPA 적용 Repository 수정

--- src/main/java/exercise/domain/Image.java
+++ src/main/java/exercise/domain/Image.java
@@ -1,14 +1,25 @@
 package exercise.domain;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = PROTECTED)
+@Entity
 public class Image {
 
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String imageUrl;
 
     public Image(String imageUrl) {
--- src/main/java/exercise/domain/ImageRepository.java
+++ src/main/java/exercise/domain/ImageRepository.java
@@ -1,19 +1,8 @@
 package exercise.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-@Component
-public class ImageRepository {
-
-    private final List<Image> images = new ArrayList<>();
-
-    public void save(Image image) {
-        images.add(image);
-    }
-
-    public List<Image> findAll() {
-        return new ArrayList<>(images);
-    }
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
 }
