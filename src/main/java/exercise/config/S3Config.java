ea026aa658d4fd89682e5bc7c2167d74d2b38cf6
Combi153 <jcm04@naver.com>
2023-11-11 20:42:44 +0900

    [refactor] credentials 정보 EC2 에서 관리하도록 변경

--- src/main/java/exercise/config/S3Config.java
+++ src/main/java/exercise/config/S3Config.java
@@ -2,36 +2,20 @@ package exercise.config;
 
 import static com.amazonaws.regions.Regions.AP_NORTHEAST_2;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
 
-    private final String accessKey;
-    private final String secretKey;
-
-    public S3Config(
-            @Value("${cloud.aws.credentials.access-key}") String accessKey,
-            @Value("${cloud.aws.credentials.secret-key}") String secretKey
-    ) {
-        this.accessKey = accessKey;
-        this.secretKey = secretKey;
-    }
-
     @Bean
     public AmazonS3 amazonS3() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
         return AmazonS3ClientBuilder.standard()
                 .withRegion(AP_NORTHEAST_2)
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withCredentials(new EC2ContainerCredentialsProviderWrapper())
                 .build();
     }
 }
