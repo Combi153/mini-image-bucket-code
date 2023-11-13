adb43fffd3548ea4e9852c7001ede90e36e3f77c
Combi153 <jcm04@naver.com>
2023-11-14 01:27:31 +0900

    [feat] memory 확인용 log 추가

--- src/main/java/exercise/presentation/SystemController.java
+++ src/main/java/exercise/presentation/SystemController.java
@@ -1,9 +1,11 @@
 package exercise.presentation;
 
 import exercise.application.SystemService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class SystemController {
 
@@ -15,6 +17,8 @@ public class SystemController {
 
     @GetMapping("/memory")
     public String readMemory() {
-        return systemService.calculateMemoryUsage();
+        String memory = systemService.calculateMemoryUsage();
+        log.info(memory);
+        return memory;
     }
 }
